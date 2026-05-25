62cdaf5ba28afd60cc27093f933d7572077efede
Sharad Bista <bishalbista737@gmail.com>
2026-05-26 00:32:55 +0545

    Add quote spider: automate login form submission and extract page content using Playwright

---
+++ quotes/quotes/spiders/quote.py
@@ -0,0 +1,37 @@
+import scrapy
+from scrapy.selector import Selector
+
+class QuoteSpider(scrapy.Spider):
+    name = "quote"
+    
+
+    def start_requests(self):
+        yield scrapy.Request(
+            "https://www.stealmylogin.com/demo",
+            meta={
+                "playwright": True,
+                "playwright_include_page": True
+            }
+        )
+
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+
+        title = await page.title()
+
+        await page.fill("input[name=username]", "random")
+        await page.fill("input[name=password]", "random")
+        await page.click("input[type=submit]")
+
+        html_content = await page.inner_html("body")
+
+        await page.close()
+
+        body = Selector(text=html_content)
+
+
+        yield{
+            "heading": body.css("h1::text").get(),
+            "paragraph": body.css("p::text").get()
+        }
+
