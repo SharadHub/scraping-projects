362ed14af3b4f6989b3282d14dc45e34c0b5d489
Sharad Bista <bishalbista737@gmail.com>
2026-05-26 22:45:46 +0545

    feat: enable RandomUserAgentMiddleware and custom proxy downloader middleware in quotes settings

--- quotes/quotes/settings.py
+++ quotes/quotes/settings.py
@@ -24,7 +24,15 @@ ADDONS = {}
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "quotes (+http://www.yourdomain.com)"
+# USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
+
+# downloader middlewares
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+    "quotes.middlewares.QuotesDownloaderMiddleware": 543
+}
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
